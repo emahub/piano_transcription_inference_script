0e029d289af6090af92b9ec670ad952db103eec6
Emahub <emahub@emahub.com>
2025-10-05 17:17:02 +0900

    update exec.sh

--- exec.sh
+++ exec.sh
@@ -1,4 +1,11 @@
 python3 -m venv venvdir
 source venvdir/bin/activate
 
+## setup
+# python3 -m pip install librosa==0.9.2
+# python3 -m pip install piano_transcription_inference
+# python3 -m pip install setuptools
+# python3 -m pip install torch
+# brew install wget
+
 python3 transcribe.py input.wav
